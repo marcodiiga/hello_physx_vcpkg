9c34fa100de6a20dfe2f70e38c325cff407251fe
Marco Alesiani <malesiani@nvidia.com>
2023-05-25 09:53:20 +0200

    test with gpu accel

--- main.cpp
+++ main.cpp
@@ -9,9 +9,41 @@ int main(int argc, char** argv) {
     PxDefaultAllocator allocator;
     PxDefaultErrorCallback error_callback;
     auto foundation = PxCreateFoundation(PX_PHYSICS_VERSION, allocator, error_callback);
-    auto physics_sdk = PxCreatePhysics(PX_PHYSICS_VERSION, *foundation, PxTolerancesScale(), true, nullptr);
+
+    // Create a CUDA context manager
+    PxCudaContextManagerDesc cudaContextManagerDesc;
+    auto cudaContextManager = PxCreateCudaContextManager(*foundation, cudaContextManagerDesc, PxGetProfilerCallback());
+
+    // Check if CUDA context manager is valid
+    if(cudaContextManager && cudaContextManager->contextIsValid()) {
+        std::cout << "GPU acceleration is available" << std::endl;
+    } else {
+        std::cout << "GPU acceleration is not available" << std::endl;
+    }
+
+    // Create a physics SDK with GPU acceleration
+    PxTolerancesScale tolerancesScale;
+    PxSceneDesc sceneDesc(tolerancesScale);
+    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+    auto dispatcher = PxDefaultCpuDispatcherCreate(4);
+    sceneDesc.cpuDispatcher = dispatcher;
+    sceneDesc.filterShader  = PxDefaultSimulationFilterShader;
+    sceneDesc.cudaContextManager = cudaContextManager;
+    sceneDesc.flags |= PxSceneFlag::eENABLE_GPU_DYNAMICS;
+    sceneDesc.broadPhaseType = PxBroadPhaseType::eGPU;
+    auto physics_sdk = PxCreatePhysics(PX_PHYSICS_VERSION, *foundation, tolerancesScale, true, nullptr);
+    auto scene = physics_sdk->createScene(sceneDesc);
+
     std::cout << "PhysX set up" << std::endl;
+
+    // Release resources
+    PX_RELEASE(scene);
+    PX_RELEASE(dispatcher);
     PX_RELEASE(physics_sdk);
     PX_RELEASE(foundation);
+    if(cudaContextManager) {
+        cudaContextManager->release();
+    }
+
     std::cout << "Shutting down.." << std::endl;
 }
